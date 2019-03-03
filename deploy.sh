5e8ea0e54a308e33b4ea406490e4e74efe44964c
Johan Wallin <j.e.wallin@gmail.com>
2019-03-03 13:14:57 +0100

    Update deploy script and config instructions

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sed -n 's/exports\.\([a-zA-Z]*\).*/\1/p' index.js | xargs -I {} gcloud beta functions deploy {} --trigger-http
+sed -n 's/exports\.\([a-zA-Z]*\).*/\1/p' index.js | xargs -I {} gcloud functions deploy {} --trigger-http
