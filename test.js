d3d6970bf9dad01bef8e138057c6bef232943249
Johan Wallin <j.e.wallin@gmail.com>
2019-03-03 12:09:01 +0100

    Report testing errors in console

--- test.js
+++ test.js
@@ -2,4 +2,7 @@ const player = require('./lib/player.js');
 
 const deviceName = process.argv[2];
 
-player.playOnDevice(deviceName);
+player
+  .playOnDevice(deviceName)
+  .then(() => console.log('Playback started successfully'))
+  .catch((err) => { console.error(err); });
