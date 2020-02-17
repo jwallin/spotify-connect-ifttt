cc23cd3d1e6ef0b69405d750ed1a2b52fccb5c78
Colin Fahy <colinfahy@spotify.com>
2020-02-16 21:13:06 -0500

    update deploy command

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sed -n 's/exports\.\([a-zA-Z]*\).*/\1/p' index.js | xargs -I {} gcloud functions deploy {} --trigger-http
+sed -n 's/exports\.\([a-zA-Z]*\).*/\1/p' index.js | xargs -I {} gcloud functions deploy {} --trigger-http --runtime nodejs10
